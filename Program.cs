37504e4c9652c195c6454339b93885c5082cdc60
SpookyDK <esben9541@gmail.com>
2023-01-15 13:03:52 +0100

    Logic

--- Program.cs
+++ Program.cs
@@ -1,19 +1,34 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
+using System.Timers;
+
+
 
 namespace Snake_test
 {
     internal class Program
     {
         static ConsoleKeyInfo KeyInfo = new ConsoleKeyInfo();
-        static char Key;
-        
+        static char Key = 'd';
+        static List<int> SnakeX = new List<int>();
+        static List<int> SnakeY = new List<int>();
+
         static void Main(string[] args)
         {
             int width = 20;
             int height = 20;
             MakeBoard(width, height);
-            Input();
+            //Input();
             WritePoint(4, 5);
+            Logic();
+            // Create a timer
+            var myTimer = new System.Timers.Timer();
+            // Tell the timer what to do when it elapses
+            myTimer.Elapsed += new ElapsedEventHandler(Logic);
+            // Set it to go off every five seconds
+            myTimer.Interval = 5000;
+            // And start it        
+            myTimer.Enabled = true;
         }
         static void MakeBoard(int width, int height)
         {
@@ -68,6 +83,50 @@ namespace Snake_test
         {
             Console.SetCursorPosition(x, y);
             Console.Write("*");
+        }
+        static void Logic()
+        {
+            SnakeX.Add(3);
+            SnakeX.Add(4);
+            SnakeY.Add(3);
+            SnakeY.Add(3);
+            SnakeX.Add(2);
+            SnakeY.Add(3);
+            SnakeX.Add(2);
+            SnakeY.Add(4);
+            SnakeX.Add(2);
+            SnakeY.Add(5);
+
+
+            for (int i = SnakeX.Count; i > 1; i--)
+            {
+                SnakeX[i - 1] = SnakeX[i - 2];
+            }
+            for (int i = SnakeY.Count; i > 1; i--)
+            {
+                SnakeY[i - 1] = SnakeY[i - 2];
+            }
+            switch (Key)
+            {
+                case ('a'):
+                    SnakeX[0]--;
+                    break;
+                case ('d'):
+                    SnakeX[0]++;
+                    break;
+                case ('w'):
+                    SnakeY[0]++;
+                    break;
+                case ('s'):
+                    SnakeY[0]--;
+                    break;
+            }
+
+            for (int i = 0; i < SnakeX.Count; i++)
+            {
+                Console.SetCursorPosition(SnakeX[i], SnakeY[i]);
+                Console.Write("a");
+            }
         }
 
     }
