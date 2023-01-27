04c9c51874b593be8efe82a2a011656021f34fad
Zadd ZC <davidbk927@gmail.com>
2023-01-27 08:21:04 +0100

    Fruit "FIxed"

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Timers;
-using System.Threading.Tasks;
 namespace SnakeConsole
 {
     internal class Program
@@ -21,50 +18,50 @@ namespace SnakeConsole
         static bool alive = true;
         static void Main(string[] args)
         {
-           Console.CursorVisible = false;
-           
-           random = new Random();
-           MakeBoard();
+            Console.CursorVisible = false;
+
+            random = new Random();
+            MakeBoard();
 
             SnakeX.Add(4);
             SnakeY.Add(3);
             SpawnFruit();
-            
+
             while (alive)
             {
-                
-                 DateTime timeStampStart = DateTime.UtcNow;
 
-                
+                DateTime timeStampStart = DateTime.UtcNow;
 
-                    
-                    Input();
-                    Logic();
-                    CollisionCheck();
-                    
-                    Console.SetCursorPosition(0, 0);
-                    Console.WriteLine(SnakeX.Count);
-                 
-                 
-                    waitFor = (int)(frameTiming * 1000 - (float)(DateTime.UtcNow - timeStampStart).TotalMilliseconds);
-                    Console.SetCursorPosition(0, height + 2);
-                    Console.WriteLine((float)(DateTime.UtcNow - timeStampStart).TotalMilliseconds + "ms");
-                    System.Threading.Thread.Sleep(waitFor);
+
+
+
+                Input();
+                Logic();
+                CollisionCheck();
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(SnakeX.Count);
+
+
+                waitFor = (int)(frameTiming * 1000 - (float)(DateTime.UtcNow - timeStampStart).TotalMilliseconds);
+                Console.SetCursorPosition(0, height + 2);
+                Console.WriteLine((float)(DateTime.UtcNow - timeStampStart).TotalMilliseconds + "ms");
+                System.Threading.Thread.Sleep(waitFor);
             }
-            
+
             Console.WriteLine(waitFor + "ms");
 
             Console.WriteLine("THIS PROGRAM WAS CREATED BY");
             Console.WriteLine("Esben & David");
 
-           
+
 
         }
 
 
         static void MakeBoard()
         {
-            
+
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
             for (int i = 2; i < width; i++)
@@ -74,7 +71,7 @@ namespace SnakeConsole
                 Console.SetCursorPosition(i, height);
                 Console.Write("_");
             }
-            
+
 
 
             for (int i = 2; i < height + 1; i++)
@@ -84,8 +81,8 @@ namespace SnakeConsole
                 Console.SetCursorPosition(width, i);
                 Console.Write("|");
             }
-            Console.ForegroundColor = ConsoleColor.Green;
-            
+            Console.ForegroundColor = ConsoleColor.Green;
+
 
         }
         static void Input()
@@ -99,47 +96,53 @@ namespace SnakeConsole
 
         static void AddTail()
         {
-            SnakeX.Add(SnakeX.Count-1);
-            SnakeY.Add(SnakeY.Count-1);
-            
+            SnakeX.Add(SnakeX.Count - 1);
+            SnakeY.Add(SnakeY.Count - 1);
+
         }
 
         static void SpawnFruit()
         {
-            fruitX = random.Next(2, width);
-            fruitY = random.Next(2, height);
-            Console.SetCursorPosition(fruitX, fruitY);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("#");
-            Console.ForegroundColor = ConsoleColor.Green;
-            
+            while (true)
+            {
+                fruitX = random.Next(2, width);
+                fruitY = random.Next(2, height);
+                if (SnakeX.Contains(fruitX) || SnakeY.Contains(fruitY))
+                {
+                    Console.SetCursorPosition(fruitX, fruitY);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("#");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return;
+                }
+            }
         }
         static void CollisionCheck()
         {
             if (SnakeX[0] < 2 || SnakeX[0] > width - 1)
-                {
-                    alive = false;
+            {
+                alive = false;
 
-                }
-                if (SnakeY[0] < 2 || SnakeY[0] > height - 1)
+            }
+            if (SnakeY[0] < 2 || SnakeY[0] > height - 1)
+            {
+                alive = false;
+            }
+            for (int i = 1; i < SnakeX.Count - 1; i++)
+            {
+                if (SnakeX[0] == SnakeX[i] && SnakeY[0] == SnakeY[i])
                 {
                     alive = false;
                 }
-                for (int i = 1; i < SnakeX.Count-1; i++)
-                {
-                    if (SnakeX[0] == SnakeX[i] && SnakeY[0] == SnakeY[i])
-                    {
-                        alive = false;
-                    }
-                }
+            }
         }
 
 
         static void Logic()
         {
-           
 
-            Console.SetCursorPosition(SnakeX[SnakeX.Count -1], SnakeY[SnakeY.Count -1]);
+
+            Console.SetCursorPosition(SnakeX[SnakeX.Count - 1], SnakeY[SnakeY.Count - 1]);
             Console.Write(' ');
             if (SnakeX[0] == fruitX && SnakeY[0] == fruitY)
             {
@@ -173,7 +176,7 @@ namespace SnakeConsole
                     SnakeY[0]++;
                     break;
             }
-            
+
             //Writes the actual snake position 
             /*for (int i = 0; i < SnakeX.Count; i++)
             {
@@ -185,6 +188,6 @@ namespace SnakeConsole
             Console.Write("¤");
         }
 
-        
+
     }
 }
